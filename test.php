df2612df4e7e65d99e9790b099aa3feb3e09c8ab
“Åsa <asaberglund54@gmail.com>
2021-10-13 14:53:19 +0200

    all classes are woking

--- test.php
+++ test.php
@@ -1,8 +1,8 @@
 <?php
 include("includes/config.php");
-$c = new Courses;
-$p = new Projects;
-
+$c = new Course;
+$p = new Project;
+$w = new Workplace;
 
 //var_dump($c->addCourse("test", "test", "test"));
 //var_dump($c->getCourse(6));
@@ -19,10 +19,21 @@ $p = new Projects;
 //var_dump($p->addProject("test", "test", "test"));
 //var_dump($p->getProject(6));
 
-$p->deleteProject(2);
+// $p->deleteProject(2);
 
 //var_dump($p->updateProject(2, "222", "222", "222", "22"));
 
+// echo "<pre>";
+// var_dump($p->getProjects());
+// echo "</pre>";
+
+//var_dump($w->addWorkplace("test", "test", "test", "test"));
+//var_dump($w->getWorkplace(1));
+
+// $w->deleteWorkplace(2);
+
+//var_dump($w->updateWorkplace(2, "222", "222", "222", "22"));
+
 echo "<pre>";
-var_dump($p->getProjects());
+var_dump($w->getWorkplaces());
 echo "</pre>";
