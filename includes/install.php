bd9d421a3e9932cae6a99567c586a85157da05cc
Åsa Berglund <you@example.com>
2021-11-03 14:13:46 +0100

    commt

--- includes/install.php
+++ includes/install.php
@@ -10,7 +10,7 @@ if ($db->connect_errno > 0) {
     die("Fel vid anlutning:" . $this->db->connect_error);
 }
 
-//Creating table for courses
+// // //Creating table for courses
 $sql ="DROP TABLE IF EXISTS courses_portfolio;";
 $sql .= "CREATE TABLE courses_portfolio(
     id INT(10) PRIMARY KEY AUTO_INCREMENT,
@@ -19,7 +19,7 @@ $sql .= "CREATE TABLE courses_portfolio(
     link VARCHAR(255) NOT NULL,
     description VARCHAR(350) NOT NULL
 );";
-// // //Adding data to the webpage for courses
+// // // // //Adding data to the webpage for courses
 $sql .="INSERT INTO courses_portfolio( university, name, link, description) VALUES ('Mittuniversitetet',  'Webbutveckling III', 'https://www.miun.se/utbildning/kurser/Sok-kursplan/kursplan/?kursplanid=21873', 'Fördjupning i JavaScript och Php för att kunna skapa interaktiva webbplatser, baserade på en automatiserad utvecklingsmiljö');";
 $sql .="INSERT INTO courses_portfolio( name, link, description, university) VALUES ( 'Projektledning', 'https://www.miun.se/utbildning/kursplaner-och-utbildningsplaner/Sok-kursplan/kursplan/?kursplanid=27003', 'Syftet med kursen är att ge grundkunskap om projektarbete och ledning av projekt. Kursen fokuseras slutligen mot praktiska studier av reella projekt och tillämpningar av projektmetodik.', 'Mittuniversitetet');";
 $sql .="INSERT INTO courses_portfolio( name, link, description, university) VALUES ('Typografi och form för webb', 'https://www.miun.se/utbildning/kursplaner-och-utbildningsplaner/Sok-kursplan/kursplan/?kursplanid=24399', 'Kursen syftar till att skapa en medvetenhet om grundläggande typografi och läslighet för skärm samt introducera praktiskt designarbete.', 'Mittuniversitetet');";
@@ -31,8 +31,23 @@ $sql .="INSERT INTO courses_portfolio(name, link, description, university) VALUE
 $sql .="INSERT INTO courses_portfolio(name, link, description, university) VALUES ('Webbutveckling I', 'https://www.miun.se/utbildning/kursplaner-och-utbildningsplaner/Sok-kursplan/kursplan/?kursplanid=18908', 'Grunder i HTML och CSS. Kursens övergripande mål är att ge god förståelse för vikten av genomtänkt design på en webbplats. Att kunna analysera de tänkta användarna, men även rent praktiskt realisera denna design i en användbar webbplats.', 'Mittuniversitetet');";
 $sql .="INSERT INTO courses_portfolio(name, link, description, university) VALUES ('Introduktion till programmering i JavaScript', 'https://www.miun.se/utbildning/kursplaner-och-utbildningsplaner/Sok-kursplan/kursplan/?kursplanid=30811', 'Grunderna för programering i JavaScript');";
 
+// //Creating tables for projects
+$sql .="DROP TABLE IF EXISTS projects_portfolio;";
+$sql .= "CREATE TABLE projects_portfolio(
+    id INT(10) PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(65) NOT NULL,
+    description VARCHAR(350) NOT NULL,
+    link VARCHAR(350) NOT NULL
+    );";
+// //Adding data to the webpage for projects
+$sql .="INSERT INTO projects_portfolio(name, description, link) VALUES ('Sveriges Radio - Rest-API', 'Hemsida med fokus på att läsa in data ifrån rest-api', 'http://minafiler.miun.se/~asbe2001/dt084g/ProjektJS/');";
+$sql .="INSERT INTO projects_portfolio(name, description, link) VALUES ('Umami - Nordiska soya', 'Mallsida med fokus på bildbehandling och grafik', 'http://minafiler.miun.se/~asbe2001/DT163G/Umami/');";
+$sql .="INSERT INTO projects_portfolio(name, description, link) VALUES ('Hemsedal Skate', 'Mallsida med fokus på typografi', 'http://minafiler.miun.se/~asbe2001/GD008G/Hemsedal-diy/');";
+$sql .="INSERT INTO projects_portfolio(name, description, link) VALUES ('Svea Gård', 'Företagshemsida med Wordpress som CMS system', 'http://asaberglund.se/wordpress_simply/');";
+$sql .="INSERT INTO projects_portfolio(name, description, link) VALUES ('Make an impact', 'Bloggportal med php', 'http://asaberglund.se/impact/');";
 
-//creating tables for workplaces
+
+// // //creating tables for workplaces
 $sql .="DROP TABLE IF EXISTS workplaces_portfolio;";
 $sql .= "CREATE TABLE workplaces_portfolio(
     id INT(10) PRIMARY KEY AUTO_INCREMENT,
@@ -40,30 +55,17 @@ $sql .= "CREATE TABLE workplaces_portfolio(
     description VARCHAR(150) NOT NULL,
     date VARCHAR(32) NOT NULL UNIQUE,
     text VARCHAR(350) NOT NULL
-);";
+    );";
 
-//Adding data to the webpage for workplaces
+// // //Adding data to the webpage for workplaces
 $sql .="INSERT INTO workplaces_portfolio( name,description, date, text) VALUES ('Harahon hotell', 'Fast anställning', '2019/12/01 - 2020/08/01', 'Jobbade som kock och som hållbarhetsansvarig för restaurangen på Harahon i Hemsedal, Norge');";
 $sql .="INSERT INTO workplaces_portfolio( name,description, date, text) VALUES ('Mandelmanns trädgårdar', 'Tidsbegränsad anställning', '2016/04/01 - 2016/08/01', 'Jobbade som kock på den ekologiska gården Mandelmanns trädgårdar där allt som serverades ifrån köket kom från gården. ');";
 $sql .="INSERT INTO workplaces_portfolio( name,description, date, text) VALUES ('Örebro Kommun', 'Projektanställning', '2018/04/01 - 2018/10/15', 'Gjorde en förstudie för Örebro kommun som undersökte hur försäljningskanalerna för lokala producenter fungerar i dagsläget samt om det finns några åtgärder kommunenn skulle kunna göra för att förbättra dessa.');";
 $sql .="INSERT INTO workplaces_portfolio( name,description, date, text) VALUES ('Aptiv', 'Sommar-praktik', '2021/04/01 - 2021/09/20', 'Praktiserade på APTIV Advanced Safety under sommaren 2021. Jobbade i agila projektteam och hjälpte till att automatisera olika arbetsprocesser. Hjälpte även till att designa ett visa kort för APRIV student-praktik');";
 
 
-// //Creating tables for projects
-$sql .="DROP TABLE IF EXISTS projects_portfolio;";
-$sql .= "CREATE TABLE projects_portfolio(
-    id INT(10) PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(32) NOT NULL,
-    description VARCHAR(200) NOT NULL,
-    link VARCHAR(32) NOT NULL,
-    );";
 
-//Adding data to the webpage for projects
-$sql .="INSERT INTO projects_portfolio(name, description, link) VALUES ('Sveriges Radio - Rest-API', 'Hemsida med fokus på att läsa in data ifrån rest-api', 'http://minafiler.miun.se/~asbe2001/dt084g/ProjektJS/');";
-$sql .="INSERT INTO projects_portfolio(name, description, link) VALUES ('Umami - Nordiska soya', 'Mallsida med fokus på bildbehandling och grafik', 'http://minafiler.miun.se/~asbe2001/DT163G/Umami/');";
-$sql .="INSERT INTO projects_portfolio(name, description, link) VALUES ('Hemsedal Skate', 'Mallsida med fokus på typografi', 'http://minafiler.miun.se/~asbe2001/GD008G/Hemsedal-diy/');";
-$sql .="INSERT INTO projects_portfolio(name, description, link) VALUES ('Svea Gård', 'Företagshemsida med Wordpress som CMS system', 'http://asaberglund.se/wordpress_simply/');";
-$sql .="INSERT INTO projects_portfolio(name, description, link) VALUES ('Make an impact', 'Bloggportal med php', 'http://asaberglund.se/impact/');";
+
 
 /* If success, print print pre tag else warning */
 if($db->multi_query($sql)) {
